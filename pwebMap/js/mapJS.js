8094d61a50ac03e1d44e856f47b1f9e072c5021a
Lwiz <77757761+LouisePrd@users.noreply.github.com>
2022-02-21 20:48:15 +0100

    V4 avec souci dans le mapjs.js
    problème avec les icones

--- pwebMap/js/mapJS.js
+++ pwebMap/js/mapJS.js
@@ -8,15 +8,33 @@ window.onload = function () {
 	//chargement des icones
 	var startIcon = L.icon({
     iconUrl: './img/début.png',
-
-    iconSize:     [35, 55], // size of the icon
+    iconSize:     [30, 50], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor	
+});
+	var endIcon = L.icon({
+    iconUrl: './img/fin.png',
+    iconSize:     [30, 50], // size of the icon
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-	
 });
 
-L.marker([51.5, -0.09], {icon: startIcon}).addTo(map).bindPopup("Point de départ");;
-}
+
+//problème à régler : quand on choisit dans la liste déroulante, pas de changement d'icone
+//car on prend la valeur qu'on a dans le lancement de la page en windows.onload
+//mais si on met map.on("click", function(e) hors du windows.onload = map pas reconnue
+$("#valeur").on('change', function()  {
+	alert(document.getElementById("valeur").value);
+	map.on("click", function(e){
+		if (document.getElementById("valeur").value = "startIcon"){
+			var mp = new L.Marker([e.latlng.lat, e.latlng.lng], {icon: startIcon}).addTo(map);
+		}
+		else{
+			var mp = new L.Marker([e.latlng.lat, e.latlng.lng], {icon: endIcon}).addTo(map);
+		}
+		alert(mp.getLatLng());
+	});
+ });
 
 
-
+};
