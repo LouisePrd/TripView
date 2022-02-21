dab7ad5b8df1cfc2b52607af018aae80e983819d
Lwiz <77757761+LouisePrd@users.noreply.github.com>
2022-02-21 19:17:11 +0100

    V2

--- pwebMap/js/mapJS.js
+++ pwebMap/js/mapJS.js
@@ -4,9 +4,18 @@ window.onload = function () {
 	var map = L.map('map').setView([14,-14.8883335],4);
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: 'PING'}).addTo(map);
 	
-	//Sur le click de la map, ajout d'un marqueur sur la carte avec le nom du pays
-	map.on('click', onClick);
+
+	//chargement des icones
+	var startIcon = L.icon({
+    iconUrl: './img/début.png',
+
+    iconSize:     [35, 55], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 	
+});
+
+L.marker([51.5, -0.09], {icon: startIcon}).addTo(map).bindPopup("Point de départ");;
 }
 
 
