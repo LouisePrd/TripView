adff2db8a545b2b01ed4d08412ff60cf623a15fe
Lwiz <77757761+LouisePrd@users.noreply.github.com>
2022-03-11 10:18:41 +0100

    ajout du bon code + ident

---
+++ TripView-main/ident_bd.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+	function verif_ident_bd($nom, $mdp, &$resultat=array()) {
+		require ('connect.php');
+		$sql="SELECT * FROM `user`  where nom=:nom and mdp=:mdp";
+
+		try {
+			$commande = $pdo->prepare($sql);
+			$commande->bindParam(':nom', $nom);
+			$commande->bindParam(':mdp', $mdp);
+			$bool = $commande->execute();
+
+			if ($bool)
+				$resultat = $commande->fetchAll(PDO::FETCH_ASSOC); //tableau d'enregistrements
+
+			if (count($resultat)== 0) return false;
+			else return true;
+		}
+
+		catch (PDOException $e) {
+			echo utf8_encode("Echec de select : " . $e->getMessage() . "\n");
+			die(); // On arrête tout.
+		}
+
+		if (count($resultat)== 0) return false;
+			else return true;
+
+	}
+
+?>
---
+++ TripView-main/js/mapJS.js
@@ -0,0 +1,207 @@
+$( function() {
+    
+    //Boite dialogue pour présenter la carte
+    $( "#dialog-message" ).dialog({
+        modal: true,
+        buttons: {
+          Commencer: function() {
+            $(this).dialog( "close" );
+          }
+        }
+      });
+
+
+	// Parametre de la carte, centrée sur la france, on utilise MapQuest avec MQ
+    let map = L.map('map', {
+        layers: MQ.mapLayer(),
+        center: [48.8509, 2.4],
+        zoom: 11
+
+        
+    });
+
+    L.geoJson(balade, {
+    
+        style: function(feature) {
+            return {
+              "color": "red",
+              "weight": 7,
+              "opacity": 0.9
+              
+            }
+          },
+          onEachFeature: function(feature, layer) {
+            layer.bindPopup("TITRE DE LA BALADE : " + feature.properties.titre_balade + "." + "  " +feature.properties.texte_intro);
+            
+          }
+        }
+        
+       
+    ).addTo(map);
+
+    
+
+
+   var trav = L.geoJson(travaux, {
+        style: function(feature) {
+            return {
+              "color": "yellow",
+              "weight": 7,
+              "opacity": 0.9
+            }
+        },
+          onEachFeature: function(feature, layer) {
+            layer.bindPopup("détail de l'impact : " + feature.properties.impact_circulation_detail + " Date début :  " + feature.properties.date_debut+ " " + "Date fin : " +feature.properties.date_fin);
+          }
+        })
+
+        var overlayMapsTrav = {
+          "Problèmes de circulation ": trav,
+         };
+        L.control.layers(null, overlayMapsTrav, {
+          collapsed: false
+        }).addTo(map);
+        
+ 
+
+   var velo = L.geoJson(velib, {
+      style: function(feature) {
+          return {
+            "color": "blue",
+            "weight": 7,
+            "opacity": 0.9
+            
+          }
+        },
+        onEachFeature: function(feature, layer) {
+          layer.bindPopup(" Nom : " + feature.properties.name + " Code de la station :  " + feature.properties.stationcode);
+          
+        }
+      })
+      var overlayMaps = {
+        "Vélib disponible à paris ": velo,
+       };
+      L.control.layers(null, overlayMaps, {
+        collapsed: false
+      }).addTo(map);
+
+
+
+
+  
+    function runDirection(start, end) {
+        
+        // recrée la map par "défaut" après la suppression du chemin
+        map = L.map('map', {
+            layers: MQ.mapLayer(),
+            center: [48.76509, 1.98921],
+            zoom: 6
+            
+        });
+        L.geoJson(balade, {
+    
+            style: function(feature) {
+                return {
+                  "color": "red",
+                  "weight": 7,
+                  "opacity": 0.9
+                }
+              },
+              onEachFeature: function(feature, layer) {
+                layer.bindPopup("TITRE DE LA BALADE : " + feature.properties.titre_balade + "." + "  " + feature.properties.texte_intro);
+              }
+            }
+            
+           
+        ).addTo(map);
+        var dir = MQ.routing.directions();
+
+        dir.route({
+            locations: [
+                start,
+                end
+            ]
+        });
+    
+        CustomRouteLayer = MQ.Routing.RouteLayer.extend({
+            createStartMarker: (location) => {
+                var varIcone;
+                var marker;
+
+                varIcone = L.icon({
+                    iconUrl: 'img/début.png',
+                    iconSize: [20, 29],
+                    iconAnchor: [10, 29],
+                    popupAnchor: [0, -29]
+                }); //on définiti l'icone
+
+                marker = L.marker(location.latLng, {icon: varIcone}).addTo(map); //on ajoute l'icone de début
+                return marker;
+            },
+
+            createEndMarker: (location) => {
+                var varIcone;
+                var marker;
+
+                varIcone = L.icon({
+                    iconUrl: 'img/fin.png',
+                    iconSize: [20, 29],
+                    iconAnchor: [10, 29],
+                    popupAnchor: [0, -29]
+                });
+
+                marker = L.marker(location.latLng, {icon: varIcone}).addTo(map); //on ajoute l'icone de fin
+                return marker;
+            }
+        });
+        
+        map.addLayer(new CustomRouteLayer({
+            directions: dir,
+            fitBounds: true
+        })); 
+
+
+
+        
+    }
+
+
+    // fonction quand on envoie le formulaire
+    function submitForm(event) {
+        event.preventDefault();
+
+        // supprimer la carte actuelle
+        map.remove();
+
+        // obtenir les données du formulaire
+        start = document.getElementById("start").value;
+        end = document.getElementById("destination").value;
+
+        // re création du chemin
+        runDirection(start, end);
+
+        // rénitialiser le formulaire
+        document.getElementById("form").reset();
+    }
+
+    // affecter le formulaire
+    const form = document.getElementById('form');
+
+    // appel de la fonction submitForm()  lors de la soumission du formulaire
+    form.addEventListener('submit', submitForm);
+
+    
+    $( "#sortable" ).sortable({
+      revert: true
+    });
+
+  $( "#draggable" ).draggable({
+      connectToSortable: "#sortable",
+      helper: "clone",
+      revert: "invalid"
+  });
+
+  $( "ul, li" ).disableSelection();
+ 
+   
+} );
